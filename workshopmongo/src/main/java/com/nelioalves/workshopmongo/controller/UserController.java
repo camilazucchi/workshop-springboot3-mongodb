1073358300b9941ab6ae96e6377a1579d1510919
camilazci <camilazucchi@outlook.com>
2024-04-09 15:50:19 -0300

    refactor: add user service to findAll method

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/controller/UserController.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.nelioalves.workshopmongo.controller;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -15,11 +15,16 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> users = new ArrayList<>(Arrays.asList(maria, alex));
+        List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
     }
 
