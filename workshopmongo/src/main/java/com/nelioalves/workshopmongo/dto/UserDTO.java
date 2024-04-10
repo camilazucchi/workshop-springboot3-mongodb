980a1c6c91c2636be0c4e16362b0f63ffeb4eb7c
camilazci <camilazucchi@outlook.com>
2024-04-10 15:26:16 -0300

    feat: add user dto to return users

---
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.nelioalves.workshopmongo.dto;
+
+import com.nelioalves.workshopmongo.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO(User user) {
+        id = user.getId();
+        name = user.getName();
+        email = user.getEmail();
+    }
+
+
+}
