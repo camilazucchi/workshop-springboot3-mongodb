5331ce5ed1aab714a0cab1e272d8d1dea34c9921
camilazci <camilazucchi@outlook.com>
2024-04-12 16:41:29 -0300

    feat: add insert and convertUserDTOToUser methods

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.services;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,16 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User ID not found"));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    /* Esse método recebe um objeto "UserDTO" como parâmetro e cria um novo objeto "User" com base nas informações
+     * fornecidas pelo "UserDTO". Ele serve para converter um objeto "UserDTO" em um objeto "User". */
+    public User convertUserDTOToUser(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 
 }
