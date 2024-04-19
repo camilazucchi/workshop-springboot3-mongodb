5d2c58df790d17b870c96d57ba7b8517fd923b91
camilazci <camilazucchi@outlook.com>
2024-04-19 10:52:57 -0300

    feat: add update method to user service

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -38,6 +38,28 @@ public class UserService {
         repository.deleteById(id);
     }
 
+    /* Tive um probleminha nesse método mas resolvi da seguinte forma:
+     * 1 - Em vez de chamar diretamente "repository.findById(user.getId())", armazenei o resultado em um "Optional<User>".
+     * 2 - Verifiquei se o "Optional<User>" contém um valor usando "isPresent()".
+     * 3 - Se estiver presente, obtém o "User" do "Optional<User>" usando "get()".
+     * 4 - Caso contrário, lança uma exceção informando que o usuário não foi encontrado. */
+    @SuppressWarnings("UnusedReturnValue")
+    public User update(User user) {
+        Optional<User> optionalUser = repository.findById(user.getId());
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+            updateData(existingUser, user);
+            return repository.save(existingUser);
+        } else {
+            throw new ObjectNotFoundException("User not found");
+        }
+    }
+
+    private void updateData(User updatedUser, User user) {
+        updatedUser.setName(user.getName());
+        updatedUser.setEmail(user.getEmail());
+    }
+
     /* Esse método recebe um objeto "UserDTO" como parâmetro e cria um novo objeto "User" com base nas informações
      * fornecidas pelo "UserDTO". Ele serve para converter um objeto "UserDTO" em um objeto "User". */
     public User convertUserDTOToUser(UserDTO userDTO) {
