6466de60a5f044c2a3f69c2b57817a51fdfe182c
camilazci <camilazucchi@outlook.com>
2024-04-19 14:19:20 -0300

    feat: instantiate two posts and save them to database using post jpa repository saveAll method

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -1,33 +1,47 @@
 package com.nelioalves.workshopmongo.config;
 
+import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     @Autowired
-    public Instantiation(UserRepository repository) {
+    public Instantiation(UserRepository repository, PostRepository postRepository) {
         this.userRepository = repository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User user1 = new User(null, "Maria Brown", "maria@gmail.com");
         User user2 = new User(null, "Alex Green", "alex@gmail.com");
         User user3 = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo! Abraços!", user1);
+        Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", user2);
+
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
+        postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
 
