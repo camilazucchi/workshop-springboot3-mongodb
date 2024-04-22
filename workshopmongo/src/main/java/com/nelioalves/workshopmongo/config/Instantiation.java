20ff3c2b31e1caecadc51e0646f92615896bd465
camilazci <camilazucchi@outlook.com>
2024-04-22 14:54:43 -0300

    refactor: change post instances

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.nelioalves.workshopmongo.config;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.AuthorDTO;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +38,12 @@ public class Instantiation implements CommandLineRunner {
         User user2 = new User(null, "Alex Green", "alex@gmail.com");
         User user3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo! Abraços!", user1);
-        Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", user2);
-
+        // Salva os usuários primeiro para que eles possuam o ID.
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo! Abraços!", new AuthorDTO(user1));
+        Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user2));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
