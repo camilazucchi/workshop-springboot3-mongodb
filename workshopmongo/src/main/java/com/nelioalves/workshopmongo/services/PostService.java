6a2e79739bdbb7d44a75e98112176696558f1146
camilazci <camilazucchi@outlook.com>
2024-04-24 09:36:08 -0300

    feat: add post service and findById method

---
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.nelioalves.workshopmongo.services;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    private final PostRepository repository;
+
+    @Autowired
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post ID not found"));
+    }
+
+}
