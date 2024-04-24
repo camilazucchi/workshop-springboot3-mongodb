ffe2c7935c4f1c235385c8ca5121a4397b8d071b
camilazci <camilazucchi@outlook.com>
2024-04-24 10:28:42 -0300

    feat: add endpoint and findByTitle method to post controller

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/controller/PostController.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/controller/PostController.java
@@ -2,12 +2,12 @@ package com.nelioalves.workshopmongo.controller;
 
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.services.PostService;
+import com.nelioalves.workshopmongo.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 /* Essa anotação marca a classe como um controlador que irá lidar com requisições HTTP em uma aplicação web. */
@@ -25,6 +25,14 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        // Decodifica o texto utilizando a classe URL dentro do pacote util:
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
