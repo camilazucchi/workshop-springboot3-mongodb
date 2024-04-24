3a16da93f07d77d10edb4f589acc9a8cedf225de
camilazci <camilazucchi@outlook.com>
2024-04-24 14:37:42 -0300

    feat: add util to convert date

--- workshopmongo/src/main/java/com/nelioalves/workshopmongo/util/URL.java
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/util/URL.java
@@ -2,6 +2,10 @@ package com.nelioalves.workshopmongo.util;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /* There was the following Sonar Warning on this utility class: "Add a private constructor to hide the implicit public
  * one". Utility classes don't save any data or state; they only contain behavior. Since I didn't add a constructor to
@@ -16,6 +20,16 @@ public class URL {
 
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 
 }
