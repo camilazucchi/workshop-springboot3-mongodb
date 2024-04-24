b26a46640a0afb70ac7b3a6b7193240468cc5dee
camilazci <camilazucchi@outlook.com>
2024-04-24 09:44:34 -0300

    feat: add comment dto

---
+++ workshopmongo/src/main/java/com/nelioalves/workshopmongo/dto/CommentDTO.java
@@ -0,0 +1,25 @@
+package com.nelioalves.workshopmongo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CommentDTO implements Serializable {
+
+    private String text;
+    private Date date;
+    private AuthorDTO author;
+
+    public CommentDTO(String text, Date date, AuthorDTO author) {
+        this.text = text;
+        this.date = date;
+        this.author = author;
+    }
+
+}
